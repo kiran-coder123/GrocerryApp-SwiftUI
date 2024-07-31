d5692ae1f766937ad5a6a15f2634f03c5294b0c3
Kiran Sonne <A13221KS@partner.bajajfinserv.in>
2024-07-31 19:30:12 +0530

    Initial Commit

---
+++ GrocerryApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrocerryApp
+//
+//  Created by Kiran Sonne on 31/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrocerryApp/GrocerryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrocerryAppApp.swift
+//  GrocerryApp
+//
+//  Created by Kiran Sonne on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GrocerryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
